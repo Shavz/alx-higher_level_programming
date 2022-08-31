cdaa91e78dfd9038c3b8f2f0366effd6f720c70e
Shavz <104979137+Shavz@users.noreply.github.com>
2022-08-31 21:14:16 +0200

    16. CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,9 @@
 #include <Python.h>
 #include <stdio.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
 /**
  * print_python_list - Prints Python lists information
  * @p: Python object
