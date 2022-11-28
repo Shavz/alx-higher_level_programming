0560fd37eb040b98c1749aab94e6485175d28afe
Shavz <104979137+Shavz@users.noreply.github.com>
2022-11-28 21:53:39 +0200

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let lang in langs) {
   console.log(langs[lang]);
 }
